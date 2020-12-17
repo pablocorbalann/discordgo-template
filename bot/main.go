19280870f044578d2a29a3697ddc4a81d049683f
pablocorbcon <pablocorbcon@gmail.com>
2020-12-17 23:56:04 +0100

    style(main): solved tab error

--- bot/main.go
+++ bot/main.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+  "flag"
+  "fmt"
   "os"
   "os/signal"
   "syscall"
 
-	"github.com/bwmarrin/discordgo"
+  "github.com/bwmarrin/discordgo"
 )
 
 var Token string
 
 func init() {
-	flag.StringVar(&Token, "t", "", "The token for your bot")
-	flag.Parse()
+  flag.StringVar(&Token, "t", "", "The token for your bot")
+  flag.Parse()
 }
 
 func main() {
-	bot, err := discordgo.New("Bot " + Token)
-	if err != nil {
+  bot, err := discordgo.New("Bot " + Token)
+  if err != nil {
     fmt.Println("[ERROR]: creating Discord session,", err)
-		return
-	}
-	bot.AddHandler(messageCreate)
+    return
+  }
+  bot.AddHandler(messageCreate)
   err = bot.Open()
-	if err != nil {
+  if err != nil {
     fmt.Println("[ERROR]: opening connection,", err)
     return
-	}
+  }
   fmt.Println("[BOT]: Running     Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	<-sc
+  sc := make(chan os.Signal, 1)
+  signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+  <-sc
   bot.Close()
 }
 
 
 func messageCreate(session *discordgo.Session, msg *discordgo.MessageCreate) {
-	if msg.Author.ID == session.State.User.ID {
-		return
-	}
-	if msg.Content == "/hello" {
-		session.ChannelMessageSend(msg.ChannelID, "Hi!")
-	}
+  if msg.Author.ID == session.State.User.ID {
+    return
+  }
+  if msg.Content == "/hello" {
+    session.ChannelMessageSend(msg.ChannelID, "Hi!")
+  }
 }
